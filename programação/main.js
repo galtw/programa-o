9365cb4dcbe3dac580f35ff7b97e8c0a4135dd18
Graziela Larissa <111892130+galtw@users.noreply.github.com>
2024-05-08 12:41:48 -0300

    Delete programação directory

--- programação/main.js
+++
@@ -1,6 +0,0 @@
-function calcularIMC() {
-    var peso = document.getElementById('peso').value;
-    var altura = document.getElementById('altura').value;
-    var imc = peso / (altura*altura);
-    document.getElementById('imc').value = imc.toFixed(2);
-}
